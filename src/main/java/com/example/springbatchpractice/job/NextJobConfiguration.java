6973ef2f2c86f6db2b939ce43cbd2c64af776979
Chung Beom Cho <cndqjacndqja@gmail.com>
2021-10-01 15:10:55 +0900

    Feat: NextJob 조건 흐름 제어 추가

--- src/main/java/com/example/springbatchpractice/job/NextJobConfiguration.java
+++ src/main/java/com/example/springbatchpractice/job/NextJobConfiguration.java
@@ -2,6 +2,7 @@ package com.example.springbatchpractice.job;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -21,15 +22,26 @@ public class NextJobConfiguration {
     public Job NextJob() {
         return jobBuilderFactory.get("NextJob")
                 .start(NextStep1())
-                .next(NextStep2())
+                    .on("FAILED") // FAILED일 경우
+                    .to(NextStep3()) // STEP2로 이동한다.
+                    .on("*") // STEP2의 결과에 상관없이
+                    .end()  // STEP2로 이동하면 FLOW가 종료한다.
+                .from(NextStep1()) // STEP1로부터
+                    .on("*") // FAILED 외에 모든 경우
+                    .to(NextStep2()) // STEP2로 이동한다.
+                    .next(NextStep3()) // STEP2가 정상적으로 끝나면 STEP3로 이동한다.
+                    .on("*") // STEP3의 결과 관계없이
+                    .end() // STEP3로 이동하면 FLOW가 종료한다.
+                .end()
                 .build();
     }
 
     @Bean
     public Step NextStep1() {
-        return stepBuilderFactory.get("Step")
+        return stepBuilderFactory.get("Step1")
                 .tasklet(((contribution, chunkContext) -> {
                     log.info("][----Next Step1----][");
+                    contribution.setExitStatus(ExitStatus.FAILED);
                     return RepeatStatus.FINISHED;
                 }))
                 .build();
@@ -37,9 +49,19 @@ public class NextJobConfiguration {
 
     @Bean
     public Step NextStep2() {
-        return stepBuilderFactory.get("Step")
+        return stepBuilderFactory.get("Step2")
                 .tasklet(((contribution, chunkContext) -> {
                     log.info("][----Next Step2----][");
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+    @Bean
+    public Step NextStep3() {
+        return stepBuilderFactory.get("Step3")
+                .tasklet(((contribution, chunkContext) -> {
+                    log.info("][----Next Step3----][");
                     return RepeatStatus.FINISHED;
                 }))
                 .build();
